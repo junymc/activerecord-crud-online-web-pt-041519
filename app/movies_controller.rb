6d7286f99d4b6c34834e9a015a19e7e249f2ed8a
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-11 16:22:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -34,7 +34,7 @@ def can_be_created_in_a_block(args = attributes)
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.ftitle.first
+  Movie.title.first
 end
 
 def can_get_the_last_item_in_the_database
